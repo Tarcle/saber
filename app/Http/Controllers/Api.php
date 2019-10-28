ced196f4561e5d7df6f468ca9c97b2e147525c3e
Tarcle <451575@naver.com>
2019-10-28 22:29:53 +0900

    1

--- app/Http/Controllers/Api.php
+++ app/Http/Controllers/Api.php
@@ -108,7 +108,34 @@ class Api extends Controller
                     'time' => $time[$j],
                 ]);
             }
-            return json_encode($list, JSON_UNESCAPED_UNICODE);
+            return json_readable_encode($list);
         }
     }
+
+    public function json_readable_encode($in, $indent = 0, $from_array = false) {
+    $_myself = __FUNCTION__;
+    $_escape = function ($str) {
+        return preg_replace("!([\b\t\n\r\f\"\\'])!", "\\\\\\1", $str);
+    };
+
+    $out = '';
+    foreach ($in as $key=>$value) {
+        $out .= str_repeat("\t", $indent + 1);
+        $out .= "\"".$_escape((string)$key)."\": ";
+        if (is_object($value) || is_array($value)) {
+            $out .= "\n";
+            $out .= $_myself($value, $indent + 1);
+        }
+        elseif (is_bool($value)) $out .= $value ? 'true' : 'false';
+        elseif (is_null($value)) $out .= 'null';
+        elseif (is_string($value)) $out .= "\"" . $_escape($value) ."\"";
+        else $out .= $value;
+        $out .= ",\n";
+    }
+    if (!empty($out)) $out = substr($out, 0, -2);
+    $out = str_repeat("\t", $indent) . "{\n" . $out;
+    $out .= "\n" . str_repeat("\t", $indent) . "}";
+
+    return $out;
+}
 }
