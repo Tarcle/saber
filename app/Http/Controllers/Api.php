86fbc6dd7415084ba8eac5c19054c0ca0a41320b
Tarcle <451575@naver.com>
2019-10-28 22:30:45 +0900

    1

--- app/Http/Controllers/Api.php
+++ app/Http/Controllers/Api.php
@@ -109,33 +109,60 @@ class Api extends Controller
                 ]);
             }
             return json_readable_encode($list);
+
+            function json_readable_encode($in, $indent = 0, $from_array = false) {
+                $_myself = __FUNCTION__;
+                $_escape = function ($str) {
+                    return preg_replace("!([\b\t\n\r\f\"\\'])!", "\\\\\\1", $str);
+                };
+
+                $out = '';
+                foreach ($in as $key=>$value) {
+                    $out .= str_repeat("\t", $indent + 1);
+                    $out .= "\"".$_escape((string)$key)."\": ";
+                    if (is_object($value) || is_array($value)) {
+                        $out .= "\n";
+                        $out .= $_myself($value, $indent + 1);
+                    }
+                    elseif (is_bool($value)) $out .= $value ? 'true' : 'false';
+                    elseif (is_null($value)) $out .= 'null';
+                    elseif (is_string($value)) $out .= "\"" . $_escape($value) ."\"";
+                    else $out .= $value;
+                    $out .= ",\n";
+                }
+                if (!empty($out)) $out = substr($out, 0, -2);
+                $out = str_repeat("\t", $indent) . "{\n" . $out;
+                $out .= "\n" . str_repeat("\t", $indent) . "}";
+
+                return $out;
+            }
         }
     }
 
     public function json_readable_encode($in, $indent = 0, $from_array = false) {
-    $_myself = __FUNCTION__;
-    $_escape = function ($str) {
-        return preg_replace("!([\b\t\n\r\f\"\\'])!", "\\\\\\1", $str);
-    };
+        $_myself = __FUNCTION__;
+        $_escape = function ($str) {
+            return preg_replace("!([\b\t\n\r\f\"\\'])!", "\\\\\\1", $str);
+        };
 
-    $out = '';
-    foreach ($in as $key=>$value) {
-        $out .= str_repeat("\t", $indent + 1);
-        $out .= "\"".$_escape((string)$key)."\": ";
-        if (is_object($value) || is_array($value)) {
-            $out .= "\n";
-            $out .= $_myself($value, $indent + 1);
+        $out = '';
+        foreach ($in as $key=>$value) {
+            $out .= str_repeat("\t", $indent + 1);
+            $out .= "\"".$_escape((string)$key)."\": ";
+            if (is_object($value) || is_array($value)) {
+                $out .= "\n";
+                $out .= $_myself($value, $indent + 1);
+            }
+            elseif (is_bool($value)) $out .= $value ? 'true' : 'false';
+            elseif (is_null($value)) $out .= 'null';
+            elseif (is_string($value)) $out .= "\"" . $_escape($value) ."\"";
+            else $out .= $value;
+            $out .= ",\n";
         }
-        elseif (is_bool($value)) $out .= $value ? 'true' : 'false';
-        elseif (is_null($value)) $out .= 'null';
-        elseif (is_string($value)) $out .= "\"" . $_escape($value) ."\"";
-        else $out .= $value;
-        $out .= ",\n";
-    }
-    if (!empty($out)) $out = substr($out, 0, -2);
-    $out = str_repeat("\t", $indent) . "{\n" . $out;
-    $out .= "\n" . str_repeat("\t", $indent) . "}";
+        if (!empty($out)) $out = substr($out, 0, -2);
+        $out = str_repeat("\t", $indent) . "{\n" . $out;
+        $out .= "\n" . str_repeat("\t", $indent) . "}";
 
-    return $out;
-}
+        return $out;
+    }
 }
