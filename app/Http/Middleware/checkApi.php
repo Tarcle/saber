ac3b38ee4f542d0f30b1ba89fcd5a7a78a56a004
Tarcle <451575@naver.com>
2019-10-30 18:40:35 +0900

    headers middleware 추가

---
+++ app/Http/Middleware/checkApi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class checkApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->header('api') != 'beatsaber') return abort(403);
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,10 @@ Route::get('/', function () {
     return "<h1>준비중입니다.</h1><a href='comparesaber'>ScoreSaber 전적 비교</a>";
 });
 
-Route::get('/api/{param}/{value?}', 'Api@list')->where(['param' => 'search|rank']);
-Route::get('/api/profile/{id}', 'Api@profile')->where(['id' => '[0-9]+']);
-Route::get('/api/{param}/{id}/{page?}', 'Api@score')->where(['param' => '(top|recent)score', 'id' => '[0-9]+', 'page' => '[0-9]+']);
+Route::get('/api/{param}/{value?}', 'Api@list')->where(['param' => 'search|rank'])->middleware(\App\Http\Middleware\checkApi::class);
+Route::get('/api/profile/{id}', 'Api@profile')->where(['id' => '[0-9]+'])->middleware(\App\Http\Middleware\checkApi::class);
+Route::get('/api/{param}/{id}/{page?}', 'Api@score')->where(['param' => '(top|recent)score', 'id' => '[0-9]+', 'page' => '[0-9]+'])->middleware(\App\Http\Middleware\checkApi::class);
 
 Route::get('/comparesaber', function() {
     return view('comparesaber');
-});
-Route::post('/comparesaber', function() {
-
 });
